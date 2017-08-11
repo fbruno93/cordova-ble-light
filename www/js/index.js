d42c4c25f2dd77aaf8ed44c77e493138f3087772
Bruno F <Bruno F>
2017-08-12 00:37:25 +0200

    save last config sending

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,38 @@
 angular.element(document).ready(function () { angular.bootstrap(document, ['app']); });
 
 angular.module('app', ['ionic'])
+.run(function($rootScope) {
+    var def = {
+        mode: 0,
+        leds: 4,
+        speed: 50,
+        red: 0,
+        green: 0,
+        blue: 255
+    };
+    var conf = localStorage.getItem('conf')
+    if (conf == null)
+        $rootScope.conf = conf;
+    else 
+        $rootScope.conf = JSON.parse(conf);
+    console.log($rootScope.conf);
+})
 .controller('ctrl', function($scope, $timeout, $ionicSideMenuDelegate, $rootScope) {
 
     var service_uuid = '0000b2f0-0000-1000-8000-00805f9b34fb';
     var characteristic_uuid = '0000b2f1-0000-1000-8000-00805f9b34fb';
     var device;
-    var color = "#000000";
 
-    $scope.conf = $rootScope.conf;
+    $scope.conf = angular.copy($rootScope.conf);
+    
+    var r = $scope.conf.red.toString(16)
+    r = r.length > 1 ? r : '0' + r;
+    var g = $scope.conf.green.toString(16)
+    g = g.length > 1 ? g : '0' + g;
+    var b = $scope.conf.blue.toString(16)
+    b = b.length > 1 ? b : '0' + b;
 
-    // $scope.conf = {
-    //     mode: 0,
-    //     leds: 4,
-    //     speed: 50,
-    //     red: 0,
-    //     green: 0,
-    //     blue: 0
-    // };
-
-    document.addEventListener("backbutton", function() {
-        localStorage.setItem("conf", JSON.stringify($scope.conf));
-    });
+    $scope.col = '#' + r + g + b;
 
     $scope.modes = [{
         name: 'Wheel Color',
@@ -65,9 +76,10 @@ angular.module('app', ['ionic'])
         color: true,
         leds: false,
         speed: false,
-    }]
+    }];
 
-    $scope.mode = $scope.modes[0];    
+    $scope.mode    = $scope.modes[$scope.conf.mode];
+    $scope.mo      = $scope.mode.name
     $scope.devices = [];
 
     $timeout(function() {
@@ -93,12 +105,12 @@ angular.module('app', ['ionic'])
         array[3] = $scope.conf.blue;
         array[4] = $scope.conf.speed;
         array[5] = $scope.conf.leds;
-        console.log(array[5]);
+
         if (device === undefined) {
             alert("please connect on device");
             $ionicSideMenuDelegate.toggleRight();
-        }
-        ble.writeWithoutResponse(device.id, service_uuid, characteristic_uuid, array.buffer, console.log, alert);
+        } else 
+            ble.writeWithoutResponse(device.id, service_uuid, characteristic_uuid, array.buffer, console.log, alert);
     };
 
     $scope.connect = function(dev) {
@@ -118,11 +130,11 @@ angular.module('app', ['ionic'])
     };
 
     $scope.changeConf = function(mode, value) {
-        console.log(mode, value);
+
         switch (mode) {
             case 'color':
-                var rgb = color.slice(1);
-                $scope.conf.red   = parseInt((rgb[0] + rgb[1]), 16);
+                var rgb = value.slice(1);
+                $scope.conf.red   = parseInt(rgb[0] + rgb[1], 16);
                 $scope.conf.green = parseInt(rgb[2] + rgb[3], 16);
                 $scope.conf.blue  = parseInt(rgb[4] + rgb[5], 16);
             break;
@@ -135,6 +147,9 @@ angular.module('app', ['ionic'])
             break;
             default:
                 $scope.conf[mode] = value
+                
         }
+
+        localStorage.setItem('conf', JSON.stringify($scope.conf));
     };
 });
